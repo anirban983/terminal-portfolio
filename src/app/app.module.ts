1d6c520a07b0a191189a20a9f39bfafda5ee252f
Anirban Kar <anirbankar983@gmail.com>
2023-03-22 02:05:47 +0530

    🔨 Update workflow file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TerminalModule } from './components/terminal/terminal.module';
 import { TitleBarModule } from './components/title-bar/title-bar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TerminalModule, TitleBarModule],
+  imports: [BrowserModule, TerminalModule, TitleBarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
