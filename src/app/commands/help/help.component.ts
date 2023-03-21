70fc79fcfe18bbb441c7dfd600002c72ec973b2d
Anirban Kar <anirbankar983@gmail.com>
2023-03-21 15:56:57 +0530

    ✨ Add help command

--- src/app/commands/help/help.component.ts
+++ src/app/commands/help/help.component.ts
@@ -6,4 +6,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./help.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HelpComponent {}
+export class HelpComponent {
+  availableCommands = [
+    { name: 'about', description: 'about Anirban kar' },
+    { name: 'clear', description: 'clear terminal' },
+    { name: 'certificates', description: 'my certifications' },
+    { name: 'cv', description: 'download my curriculum vitae' },
+    { name: 'education', description: 'my eudcation background' },
+    { name: 'email', description: 'send an email to me' },
+    { name: 'gui', description: 'go to my portfolio in GUI' },
+    { name: 'help', description: 'check available commands' },
+    { name: 'history', description: 'view command histroy' },
+    { name: 'interests', description: 'my interests and hobbies' },
+    { name: 'projects', description: 'my personal projects' },
+    { name: 'socials', description: 'check out my social accounts' },
+    { name: 'skills', description: 'my technical skills' },
+    { name: 'themes', description: 'check available themes' },
+    { name: 'work', description: 'my work experience' }
+  ];
+}
