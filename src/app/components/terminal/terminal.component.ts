b6c464e54f0aca48902d33b319538f130eaed877
Anirban Kar <anirbankar983@gmail.com>
2023-03-21 14:26:09 +0530

    🔥 Remove run command component and move the code to terminal component

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -23,18 +23,17 @@ export class TerminalComponent {
     }
   ];
 
-  executeCommand(commandItem: ICommandItem): void {
-    const found = this.commandItems.find((item) => item.id === commandItem.id);
-    if (found) {
-      found.disableInput = true;
-      found.exists = this.checkIfCommandExists(found.command);
-      this.commandItems.push({
-        id: this.commandItems.length,
-        disableInput: false,
-        command: ECommandType.EMPTY,
-        exists: true
-      });
-    }
+  executeCommand(commandItem: ICommandItem, event: KeyboardEvent): void {
+    commandItem.disableInput = true;
+    commandItem.command = (event.target as HTMLInputElement).value as ECommandType;
+    commandItem.exists = this.checkIfCommandExists(commandItem.command);
+
+    this.commandItems.push({
+      id: this.commandItems.length,
+      disableInput: false,
+      command: ECommandType.EMPTY,
+      exists: true
+    });
   }
 
   checkIfCommandExists(command: ECommandType): boolean {
